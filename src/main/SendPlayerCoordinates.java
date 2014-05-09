f75011dad25fced002ae2526f5b553aee741600b
wazy <biobud74@yahoo.com>
2014-05-09 13:04:28 -0400

    Some updates to receiving player updates

--- src/main/SendPlayerCoordinates.java
+++ src/main/SendPlayerCoordinates.java
@@ -14,13 +14,29 @@ public class SendPlayerCoordinates {
 		int position = -1;
 
 		try {
+
+			int[] placeHolder = new int[3];
+			outputStream.writeObject(Player.onlinePlayers);
+			outputStream.flush();
+
 			while (true) {
 
+				int n = Player.onlinePlayers.size();
+
 				position = inputStream.read();
+				outputStream.write(n);
 
 				// reset so we don't write cached players
 				outputStream.reset();
-				outputStream.writeObject(Player.onlinePlayers);
+				for (int i = 0; i < n; i++) {
+					if (i != position) {
+						Player play = Player.onlinePlayers.get(i);
+						placeHolder[0] = play.getID();
+						placeHolder[1] = play.getX();
+						placeHolder[2] = play.getY();
+						outputStream.writeObject(placeHolder);
+					}
+				}
 				outputStream.flush();
 
 				// pause between updates
